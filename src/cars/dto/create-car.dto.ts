559e93b7a0edf74d2f9a9635fd9ac0f8fef542c7
rodriguez yasmani <rodriguez.yasmani@hotmail.com>
2022-01-02 02:58:10 -0300

    ajustes varios

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,7 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCarDto {
+  @ApiProperty()
   id: number;
+
+  @ApiProperty()
   plate: string;
+
+  @ApiProperty()
   brand: string;
+
+  @ApiProperty()
   model: string;
+
+  @ApiProperty()
   year: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { doc } from 'prettier';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Cars example')
+    .setDescription('The cars API description')
+    .setVersion('1.0')
+    .addTag('cars')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('restfull', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
